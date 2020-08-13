8f348c54b5867f952f59030f4842aeec71812025
rahul121290 <sinha.rahulsinha.sinha@gmail.com>
2020-08-13 14:03:55 +0530

    data inserted

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:82/pti/';
+//$config['base_url'] = 'http://localhost:82/pti/';
+$config['base_url'] = 'http://192.168.25.184:82/pti/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Pti.php
+++ application/controllers/Pti.php
@@ -3,23 +3,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pti extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+//         $db2 = $this->load->database('sqlsvr', TRUE);
+        
+        $this->load->library(['form_validation','session','email']);
+        //$this->load->model(array('Channel_model'));
+        $this->load->helper(['url', 'language','download']);
+    }
+    
+	public function index(){
 		$this->load->view('welcome_message');
+	}
+	
+	public function pti_json(){
+	    $results = json_decode(file_get_contents("http://localhost:82/ptijson.json"),true);
+	    
+	    $newsList = array();
+	    
+	    foreach($results['items'] as $result){
+	        $temp = array();
+	        
+	        $temp['guid'] = $result['guid'];
+	        $temp['link'] = $result['link'];
+	        $temp['slug_hindi'] = $result['Slug'];
+	        $temp['slug_eng'] = '';
+	        $temp['origin_hindi'] = $result['origin'];
+	        $temp['origin_eng'] = '';
+	        $temp['content'] = $result['content'];
+	        $temp['summary'] = $result['summary'];
+	        $temp['author'] = 101;
+	        $temp['published'] = $result['published'];
+	        $temp['updated'] = $result['updated'];
+	        $temp['categories'] = $result['categories'][0];
+	        $temp['sub_headline'] = $result['sub_headline'];
+	        $temp['status'] = 1;	        
+	        $newsList[] = $temp;
+	    }
+	    
+	    $this->db->insert_batch('ibc_news_pti',$newsList);
 	}
 }
