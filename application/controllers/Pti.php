a5214e382497a34791c9aef0e5b36da236ae1d9f
rahul121290 <sinha.rahulsinha.sinha@gmail.com>
2020-08-14 11:24:19 +0530

    184

--- application/controllers/Pti.php
+++ application/controllers/Pti.php
@@ -28,7 +28,7 @@ class Pti extends CI_Controller {
 	        $temp['link'] = $result['link'];
 	        $temp['slug_hindi'] = $result['Slug'];
 	        $temp['slug_eng'] = '';
-	        $temp['origin_hindi'] = $result['origin'];
+	        $temp['origin_hindi'] = trim($result['origin'],'.');
 	        $temp['origin_eng'] = '';
 	        $temp['content'] = $result['content'];
 	        $temp['summary'] = $result['summary'];
@@ -39,14 +39,18 @@ class Pti extends CI_Controller {
 	        $temp['sub_headline'] = $result['sub_headline'];
 	        $temp['status'] = 1;	
 	        $Detail = $this->Pti_model->getIbcCategory($result['categories'][0],$result['origin']);
-	        
-	        if(count($result)>0){
-	           $temp['ibc_category'] = $result['ibc_category'];
-	           $temp['ibc_news_type'] = $result['ibc_news_type'];
+	        if(count($Detail)>0){  
+	           $temp['ibc_category'] = $Detail['ibc_category'];
+	           $temp['ibc_news_type'] = $Detail['ibc_news_type'];
+	           $temp['ibc_country_id'] = $Detail['ibc_country_id'];
+	           $temp['country'] = $Detail['country'];
+	           $temp['city_id'] = $Detail['city_id'];
+	           $temp['city'] = $Detail['city'];
+	           $temp['state_id'] = $Detail['state_id'];
+	           $temp['state'] = $Detail['state'];
 	        }
 	        $newsList[] = $temp;
 	    }
-	    
 	    $this->db->insert_batch('ibc_news_pti',$newsList);
 	}
 	
--- application/models/Pti_model.php
+++ application/models/Pti_model.php
@@ -18,42 +18,66 @@ class Pti_model extends CI_Model {
 	
 	function getIbcCategory($cate,$city){
 	    $temp = array();
+	    
+	    $temp['ibc_category'] = '94';
+	    $temp['ibc_country_id']  = '101';
+	    $temp['country'] = 'इंडिया';
+		$temp['state_id'] = NULL;
+		$temp['state'] = NULL;
+		$temp['city_id'] = NULL;
+	    $temp['city'] = NULL;
+	    
 	    if($cate == 'VID'){
-	        $temp['ibc_category'] = '4';
-	        $temp['ibc_news_type'] = '6';
+	        $temp['ibc_news_type'] = '14';   
 	    }
 	    else if($cate == 'KHL'){
-	        $temp['ibc_category'] = '5';
-	        $temp['ibc_news_type'] = '6';
+	        $temp['ibc_news_type'] = '16';
 	    }
 	    else if($cate == 'ART'){
-	        $temp['ibc_category'] = '6';
-	        $temp['ibc_news_type'] = '6';
+	        $temp['ibc_news_type'] = '17';
 	    }
+	    else if($cate == 'SNS'){
+	        $temp['ibc_news_type'] = '12';
+	    }
+	    // else if($cate == 'DEL'){
+	        // //$temp['ibc_news_type'] = '6';
+	    // }
 	    else {
-	        $city = trim($city,'.'); 
+			$city = trim($city,'.');
 	        $result = $this->db->query("select state_short_code from ibc_cities where city_name_hindi = '$city'")->result_array();
+	        
 	        if(count($result)>0){
 	            if($result[0]['state_short_code'] == 'MP'){
-	                $temp['ibc_category'] = '30';
-	                $temp['ibc_news_type'] = '6';
+	                $temp['ibc_news_type'] = '18';
+	                $temp['state_id'] = '21';
+	                $temp['state'] = 'मध्य प्रदेश';
 	            }
 	            else if($result[0]['state_short_code'] == 'CG'){
-	                $temp['ibc_category'] = '29';
-	                $temp['ibc_news_type'] = '6';
+	                $temp['ibc_news_type'] = '19';
+					$temp['state_id'] = '7';
+					$temp['state'] = 'छत्तीसगढ़';
 	            }
 	            else if($result[0]['state_short_code'] == 'UP'){
-	                $temp['ibc_category'] = '291';
-	                $temp['ibc_news_type'] = '6';
+	                $temp['ibc_news_type'] = '38';
+					$temp['state'] = 'उत्तर प्रदेश';
 	            }
 	            else if($result[0]['state_short_code'] == 'MH'){
-	                $temp['ibc_category'] = '292';
-	                $temp['ibc_news_type'] = '6';
-	            } else {
-	                $temp['ibc_category'] = '3';                   //national
-	                $temp['ibc_news_type'] = '6';
+	                $temp['ibc_news_type'] = '22';
+					$temp['state'] = 'महाराष्ट्र';
+	            }
+	            else if($result[0]['state_short_code'] == 'BR'){
+	                $temp['ibc_news_type'] = '5';
+					$temp['state'] = 'बिहार';
+	            }else {
+	                $temp['ibc_news_type'] = '13';                   //national
+					$temp['state'] = NULL;
 	            }
 	        }
+			else {
+				$temp['ibc_news_type'] = '13';                   //national
+				$temp['state'] = NULL;
+			}
+			
 	    }
 	    
 	    return $temp;
